468b2a4b4fe427aa33f33286f4d5e61dadb348a7
samcyn <samsoniyanda@outlook.com>
2025-01-14 00:36:11 +0000

    test: testing web component

---
+++ web/frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+	<ui-nav-menu>
+		<a href="/" rel="home">Home</a>
+		<a href="/templates">Templates</a>
+		<a href="/settings">Settings</a>
+	</ui-nav-menu>
+</template>
