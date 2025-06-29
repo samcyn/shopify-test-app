28f313f18361c7b627f6b21ed42e4528d6618989
Samson Iyanda <samson@judge.me>
2025-06-29 01:56:15 +0100

    chore: add typescript vue project

---
+++ web/vue-test-project/src/components/shared/NavMenuItem/index.vue
@@ -0,0 +1,34 @@
+<script lang="js" setup>
+import { useRouter } from 'vue-router';
+
+defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+  rel: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+const router = useRouter();
+
+const onClick = (e) => {
+  const href = e.target.dataset.href;
+  if (href) {
+    history.pushState({}, "", href);
+    router.push(href);
+  } else {
+    history.pushState({}, "", '/');
+    router.push('/');
+  }
+}
+</script>
+<template>
+  <a :href="to" :rel="rel" :data-href="to" @click.prevent="onClick">
+    {{ title }}
+  </a>
+</template>
